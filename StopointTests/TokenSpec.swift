ab8449c279fb8be4583a9c51185f51f695157043
BeatrizLdS <beatriz_.silva@hotmail.com>
2022-10-24 08:24:29 -0300

    :test_tube: test: Feito teste do updateAccessToken

--- StopointTests/TokenSpec.swift
+++ StopointTests/TokenSpec.swift
@@ -22,10 +22,22 @@ final class TokenSpec: XCTestCase {
         tokenService.generateAccessToken()
         XCTAssertEqual("newToken", tokenService.account.token)
     }
-    func testGenerateTokenWithErrorSetsError() {
+    func testGenerateTokenWithError() {
         mockTokenService.result = .failure(NSError(domain: "", code: -1, userInfo: nil))
         tokenService.generateAccessToken()
         XCTAssertNotEqual("newToken", tokenService.account.token)
 //        XCTAssertNotNil(tokenService.error)
+    }
+    func testgetTokenInformationWithCorrectDetailsSetsExpireTokenTime() {
+        mockTokenService.result = .success(mockTokenService.generateTokenSuccesData())
+        let timeBefore = tokenService.account.expireTokenTime
+        tokenService.updateTokenInformation()
+        XCTAssertNotEqual(timeBefore, tokenService.account.expireTokenTime)
+    }
+    func testgetTokenInformationWithError() {
+        mockTokenService.result = .failure(NSError(domain: "", code: -1, userInfo: nil))
+        let timeBefore = tokenService.account.expireTokenTime
+        tokenService.updateTokenInformation()
+        XCTAssertEqual(timeBefore, tokenService.account.expireTokenTime)
     }
 }
